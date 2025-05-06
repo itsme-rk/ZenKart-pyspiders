0e4661d9eab993ba52d0a56049583e87bc4f5301
itsme-rk <karthikramesh1813@gmail.com>
2025-05-06 21:22:06 +0530

    half completed

--- script.js
+++ script.js
@@ -3,13 +3,57 @@ let nav = document.getElementById('navbar')
 let close = document.getElementById('close')
 
 if(bar){
-    bar.addEventListener("click",()=>{
+    bar.addEventListener("click", () => {
         nav.classList.toggle("active")
     })
 }
 
 if(close){
-    close.addEventListener("click",()=>{
+    close.addEventListener("click", () => {
         nav.classList.remove("active")
     })
+}
+
+var nextBtn = document.querySelector('.next'), 
+    prevBtn = document.querySelector('.prev'), 
+    carousel = document.querySelector('.carousel'), 
+    list = document.querySelector('.list'),  
+    item = document.querySelectorAll('.item') 
+
+let timeRunning = 3000  
+let timeAutoNext = 7000 
+
+nextBtn.onclick = function(){ 
+    showSlider('next') 
+} 
+
+prevBtn.onclick = function(){ 
+    showSlider('prev') 
+} 
+
+let runTimeOut  
+let runNextAuto = setTimeout(() => { 
+    nextBtn.click() 
+}, timeAutoNext) 
+
+function showSlider(type) { 
+    let sliderItemsDom = list.querySelectorAll('.carousel .list .item') 
+    if(type === 'next'){ 
+        list.appendChild(sliderItemsDom[0]) 
+        carousel.classList.add('next') 
+    } else{ 
+        list.prepend(sliderItemsDom[sliderItemsDom.length - 1]) 
+        carousel.classList.add('prev') 
+    } 
+
+    clearTimeout(runTimeOut) 
+    runTimeOut = setTimeout(() => { 
+        carousel.classList.remove('next') 
+        carousel.classList.remove('prev') 
+    }, timeRunning) 
+
+    clearTimeout(runNextAuto) 
+    runNextAuto = setTimeout(() => { 
+        nextBtn.click() 
+    }, timeAutoNext) 
 }
